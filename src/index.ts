1befc88e89895ac38a2e43368f82c42716b38fe2
lbexp <fiqarapr@gmail.com>
2026-01-26 22:52:01 +0700

    feat: create retrieve data and render ui effects

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,124 @@
-import { Console, Effect } from 'effect';
+import { Effect, pipe } from 'effect';
 
-const program = Console.log('Hello World!');
+/*
+ * Start - Type declarations
+ */
 
-Effect.runSync(program);
+interface Data {
+  blacklist: string[];
+  active: boolean;
+}
+
+/*
+ * End - Type declarations
+ */
+
+/*
+ * Start -- Const declarations
+ */
+
+const BLACKLIST_STORAGE_KEY = 'blacklist';
+const ACTIVE_STORAGE_KEY = 'active';
+
+/*
+ * End -- Const declarations
+ */
+
+/*
+ * Non-effect functions
+ */
+
+const addUrl = () => {
+  const list = document.getElementById('blacklist');
+  const input = document.getElementById('url-input') as HTMLInputElement;
+  const item = document.createElement('p');
+
+  if (input && list) {
+    item.textContent = input.value;
+    list.appendChild(item);
+  }
+};
+
+/*
+ * End -- Non-effect functions
+ */
+
+/*
+ * Start -- Effects
+ */
+
+const retrieveData = (): Effect.Effect<Data> =>
+  Effect.promise(async () => {
+    const result = await chrome.storage.local.get([BLACKLIST_STORAGE_KEY]);
+
+    try {
+      const blacklist = JSON.parse(result[BLACKLIST_STORAGE_KEY]) as string[];
+      const active = JSON.parse(result[ACTIVE_STORAGE_KEY]) as boolean;
+
+      return {
+        blacklist,
+        active,
+      };
+    } catch {
+      console.error('Failed to parse data');
+      return {
+        blacklist: [],
+        active: false,
+      };
+    }
+  });
+
+const render = (data: Data): Effect.Effect<boolean, Error> => {
+  const container = document.getElementById('app');
+
+  if (container) {
+    const { blacklist, active } = data;
+    const urlWrapper = document.createElement('div');
+    urlWrapper.setAttribute('class', 'url-wrapper');
+
+    const urlInput = document.createElement('input');
+    urlInput.setAttribute('id', 'url-input');
+    urlInput.setAttribute('class', 'url-input');
+
+    const urlAdd = document.createElement('button');
+    urlAdd.setAttribute('id', 'url-add');
+    urlAdd.setAttribute('class', 'url-add');
+    urlAdd.addEventListener('click', addUrl);
+
+    urlWrapper.appendChild(urlInput);
+    urlWrapper.appendChild(urlAdd);
+    container.appendChild(urlWrapper);
+
+    const list = document.createElement('div');
+    list.setAttribute('id', 'blacklist');
+    list.setAttribute('class', 'blacklist');
+
+    blacklist.forEach((value) => {
+      const item = document.createElement('p');
+      item.textContent = value;
+      list.appendChild(item);
+    });
+    container.appendChild(list);
+
+    const onOffWrapper = document.createElement('div');
+    onOffWrapper.setAttribute('class', 'on-off-wrapper');
+    const onOffButton = document.createElement('button');
+    onOffButton.setAttribute('id', 'on-off');
+    onOffButton.setAttribute('class', 'on-off-button');
+    onOffWrapper.appendChild(onOffButton);
+    onOffButton.classList.add(active ? 'on' : 'off');
+    onOffButton.textContent = active ? 'ON' : 'OFF';
+    container.appendChild(onOffWrapper);
+
+    return Effect.succeed(true);
+  }
+
+  return Effect.fail(new Error("Can't find the app container"));
+};
+
+/*
+ * End -- Effects
+ */
+
+// const program = pipe(Effect.flatMap(retrieveData), Effect.flatMap(render));
+// Effect.runSync(program);
